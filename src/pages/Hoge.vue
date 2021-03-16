5b02a069336c19baafa8e207307ee5ed2e19e506
Ryo-Nakano <matsuhara.9999ryo@gmail.com>
2021-03-17 01:53:28 +0900

    ユーザーの入力情報を元にtweetされるように

--- src/pages/Hoge.vue
+++ src/pages/Hoge.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>This is Hoge</h1>
+    <input type="text" v-model="tweetText"> 
     <button v-on:click="create">Submit</button>
   </div>
 </template>
@@ -9,12 +10,28 @@
 import firebase from "firebase";
 
 export default {
+  data(){
+    return {
+      col: firebase.firestore().collection("tweets"),
+
+      tweetText: "",
+    }
+  },
+
   methods: {
     create() {
-      firebase.firestore().collection("tweets").add({
-        text: "こんにちは、ツイートの本文です。"
+      const vm = this
+      this.col.add({
+        text: this.tweetText,
+      })
+      .then(function(){ //tweetのFirestoreへの保存がうまくいったとき
+        alert("Tweet completed !!")
+        vm.tweetText = ""
+      })
+      .catch(function(error){ //tweetのFirestoreへの保存に失敗したとき
+        alert(error)
       });
-    }
+    },
   }
 };
 </script>
