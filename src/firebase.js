91147d42adb7eff7f5856a531fdead80dc100aa7
promin3737 <s.s.hayabusa3737@icloud.com>
2021-03-18 16:21:56 +0900

    FireStore 修正版

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCThtCJlGGngeIBhYdyldjuzBnY6gIrAY0",
-  authDomain: "firestorepractice-d8dbf.firebaseapp.com",
-  databaseURL: "https://firestorepractice-d8dbf.firebaseio.com",
-  projectId: "firestorepractice-d8dbf",
-  storageBucket: "firestorepractice-d8dbf.appspot.com",
-  messagingSenderId: "254606928001",
-  appId: "1:254606928001:web:41a60f43d47268e2118580"
+  apiKey: "AIzaSyAY9WemiK0nuIXqSX6LYxwYmGUxMlhlEIs",
+  authDomain: "p-doctor-s-task-app.firebaseapp.com",
+  projectId: "p-doctor-s-task-app",
+  storageBucket: "p-doctor-s-task-app.appspot.com",
+  messagingSenderId: "40582126818",
+  appId: "1:40582126818:web:cd60ab8cbf3a06b10bd2bb",
+  measurementId: "G-7Q3VYMHLPW",
 };
 
 firebase.initializeApp(firebaseConfig);
