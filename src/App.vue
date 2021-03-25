ae67ebb3b8046f26b7512f626a32c3930446ce2c
Ryo-Nakano <matsuhara.9999ryo@gmail.com>
2021-03-26 07:09:19 +0900

    feat: Googleログイン実装(未ログイン時はリダイレクト)

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div id="nav">
       <router-link to="/home">Home</router-link> |
       <router-link to="/hoge">Hoge</router-link> |
-      <router-link to="/auth">UserAuth</router-link> |
+      <!-- <router-link to="/auth">UserAuth</router-link> | -->
+      <router-link to="/signin">SignIn</router-link> |
+
 
     </div>
     <router-view></router-view>
