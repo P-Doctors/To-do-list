3949248630643718f10829dd8db164ab331e6b32
promin3737 <s.s.hayabusa3737@icloud.com>
2021-03-27 14:58:20 +0900

    MyPageのデザインほぼ完成

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Home from "../pages/Home.vue";
 import MyPage from "../pages/MyPage.vue";
 import UserAuth from "../pages/UserAuth.vue";
 import SignIn from "../pages/SignIn.vue";
+import SelectUser from "../pages/SelectUser.vue";
 
 Vue.use(VueRouter);
 
@@ -14,6 +15,7 @@ const routes = [
   { path: "/mypage", name: "MyPage", component: MyPage },
   { path: "/auth", name: "UserAuth", component: UserAuth },
   { path: "/signin", name: "SignIn", component: SignIn },
+  { path: "/selectuser", name: "SelectUser", component: SelectUser },
 ];
 
 const router = new VueRouter({
